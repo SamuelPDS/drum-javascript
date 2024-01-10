d3b77271918e6e0e1544d26318f3ca607f04500d
Samuel <duartes607@gmail.com>
2024-01-10 16:38:51 -0300

    ferac: change some codes

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
 function playKey(idKey) {
     const element = document.querySelector(idKey);
-    if(element){
+    if (element) {
         element.play();
+    } else {
+        console.error('element nof found')
     }
-        else{
-            console.error('element nof found')
-        }
-    
 }
 
 const btns = document.querySelectorAll('.btn')
+for (i = 0; i < btns.length; i++) {
+    const key = btns[i];
+    const keyCode = key.classList[1];
+    const idKey = `#btn-${keyCode}`;
 
-    for (i = 0; i < btns.length; i++) {
-        const key = btns[i];
-        const keyCode = key.classList[1];
-        // console.log(key.classList[1]);
-        const idKey = `#btn-${keyCode}`;
-        // console.log(idKey)
-    
-        key.onclick = function() {
-        //  console.log(key)    
+    key.onclick = function () {
         playKey(idKey);
-        }
-        
-    
-        key.onkeydown = e => {
-            // console.log(e)
-            if(e.key === 'e' || e.key === 'r' ){
-                playKey(idKey)
-            }
-        }
-    
     }
+
+    key.onkeydown = e => {
+        if (e.key === 'e' || e.key === 'r') {
+            playKey(idKey)
+        }
+    }
+
+}
 
 
 
