c7d36039fa0f4ca2e247af749de7f15e106cc1f0
Samuel <duartes607@gmail.com>
2024-01-11 19:48:48 -0300

    feat: add the final code

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-document.addEventListener('keydown', (e) => {
-    const key = e.key.toLowerCase();
-
-    // if(key === 'e'){
-    //     playKey(idKey)
-    // }
+const btns = document.querySelectorAll('.btn')
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        const idKey = btn.getAttribute('data-key');
+       playKey(idKey); 
+    }) 
+})
 
     function playKey(idKey) {
-        const element = document.querySelector(idKey);
-        if (element) {
-            element.play();
+        const audio = document.querySelector(`audio[data-key="${idKey}"]`);
+        if (audio) {
+            audio.currentTime = 0;
+            audio.play();
         } else {
-            console.error('element nof found')
+            console.error('element not found')
         }
     }
     
-    const btns = document.querySelectorAll('.btn')
-    for (i = 0; i < btns.length; i++) {
-        const key = btns[i];
-        const keyCode = key.classList[1];
-        const idKey = `#btn-${keyCode}`;
-    
-        key.onclick = function () {
-            playKey(idKey);
-        }
-    
-        key.onkeydown = e => {
-            if (e.key === 'e' || e.key === 'r') {
-                playKey(idKey)
-            }
-        }
-    
-    }
+    document.addEventListener('keydown', (e) => {
+        const idKey = e.keyCode.toString();
+        playKey(idKey)    
 })
+
+
+    // for (i = 0; i < btns.length; i++) {
+    //     const key = btns[i];
+    //     const keyCode = key.classList[1];
+    //     const idKey = `#btn-${keyCode}`;
+        
+    //     key.onclick = function () {
+    //         playKey(idKey);
+    //     }
+    
+    //     key.onkeydown = e => {
+    //         console.log(e)
+    //         if (e.key === 'e' || e.key === 'r') {
+    //             playKey(idKey)
+    //         }
+    //     }
+    
+    // }
+
 
 
 
