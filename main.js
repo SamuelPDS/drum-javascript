bfe16157093aea638e53f234cdd32eed95d6b2ae
Samuel <duartes607@gmail.com>
2024-01-11 19:24:09 -0300

    refac: tried to resolve the codes

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-function playKey(idKey) {
-    const element = document.querySelector(idKey);
-    if (element) {
-        element.play();
-    } else {
-        console.error('element nof found')
-    }
-}
+document.addEventListener('keydown', (e) => {
+    const key = e.key.toLowerCase();
 
-const btns = document.querySelectorAll('.btn')
-for (i = 0; i < btns.length; i++) {
-    const key = btns[i];
-    const keyCode = key.classList[1];
-    const idKey = `#btn-${keyCode}`;
+    // if(key === 'e'){
+    //     playKey(idKey)
+    // }
 
-    key.onclick = function () {
-        playKey(idKey);
-    }
-
-    key.onkeydown = e => {
-        if (e.key === 'e' || e.key === 'r') {
-            playKey(idKey)
+    function playKey(idKey) {
+        const element = document.querySelector(idKey);
+        if (element) {
+            element.play();
+        } else {
+            console.error('element nof found')
         }
     }
+    
+    const btns = document.querySelectorAll('.btn')
+    for (i = 0; i < btns.length; i++) {
+        const key = btns[i];
+        const keyCode = key.classList[1];
+        const idKey = `#btn-${keyCode}`;
+    
+        key.onclick = function () {
+            playKey(idKey);
+        }
+    
+        key.onkeydown = e => {
+            if (e.key === 'e' || e.key === 'r') {
+                playKey(idKey)
+            }
+        }
+    
+    }
+})
 
-}
+
 
 
 
